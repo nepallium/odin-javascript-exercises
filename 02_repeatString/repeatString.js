6df60f53d29e8c934601b5f2b2dfc83c515c6bc7
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-16 12:19:52 -0500

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    let repeatedStr = "";
 
+    if (n >= 0) {
+        for (let i = 0; i < n; i++) {
+            repeatedStr += str
+        }
+    }
+    else {
+        repeatedStr = "ERROR";
+        // OR just return "ERROR";
+    }
+
+    return repeatedStr;
 };
 
 // Do not edit below this line
