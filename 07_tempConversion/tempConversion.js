c4dcc9f3ab0e0efc84a9e4e66537154ceb56e590
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-16 14:23:46 -0500

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit-32) * 5/9;
+  if (Number.isInteger(celcius)) {
+    return celcius;
+  }
+  return +celcius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = celcius*(9/5) + 32;
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  }
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
