92623584f49ecff0adeb4e6c108f19bcca208962
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-16 14:03:43 -0500

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (typeof firstNum !== "number" ||
+        typeof secondNum !== "number" ||
+        firstNum < 0 || secondNum < 0 ||
+        !Number.isInteger(firstNum) ||
+        !Number.isInteger(secondNum)
+    ) {
+        return "ERROR"
+    }
 
+    start = firstNum < secondNum ? firstNum : secondNum;
+    end = firstNum > secondNum ? firstNum : secondNum;
+
+    let length = end-start + 1;
+    return (length*(start+end)) / 2;
 };
 
 // Do not edit below this line
