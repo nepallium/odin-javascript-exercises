89a91ea272ff55dfd0aafc705111c54daafcc46c
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-16 13:31:57 -0500

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (element of itemsToRemove) {
+        for (let i = 0; i < array.length; i++) {
+            // Delete element if it matches with current index in array
+            if (array[i] === element) {
+                array.splice(i, 1) // delete 1 element at index i 
+                i--; // go back 1 index since an element has been deleted
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
