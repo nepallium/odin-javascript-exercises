f764f7498816db4aedd4e7cf5cc5e3e9474d4fa1
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-24 17:39:55 -0500

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, num) => total + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponenant) {
+	return Math.pow(base,exponenant);
 };
 
-const factorial = function() {
-	
+const factorial = function(start) {
+  let fact = 1;
+	for (let i = start; i > 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
