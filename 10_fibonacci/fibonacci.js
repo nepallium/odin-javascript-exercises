dfdbbd12e6a0d6067cdde93c0a7c66553bfbe206
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-24 19:14:47 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let arr = [0,1]
+    for (let i = 2; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
 
+    return arr[n];
 };
 
 // Do not edit below this line
