480f3767c119d25b78121fea0bb1e82c5d7e3725
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-24 19:05:53 -0500

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    // Remove all non-alphanumeric characters
+    let converted = [...str]
+                .filter((char, index) => (str.charCodeAt(index) >= 48 && str.charCodeAt(index) <= 57) ||
+                    (str.charCodeAt(index) >= 97 && str.charCodeAt(index) <= 122))
+                .join("");
+    
+    for (let i = 0; i < converted.length / 2; i++) {
+        if (converted.charAt(i) !== converted.charAt(converted.length-i-1)) {
+            return false;
+        }
+    }
+    return true;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
