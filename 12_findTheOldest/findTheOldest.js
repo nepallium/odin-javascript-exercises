eaf9643e80240b236b7aa8910b74dcba9b517555
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-24 19:37:55 -0500

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, person) => {
+        let currentYear = new Date().getFullYear();
+        oldYearOfDeath = oldest.yearOfDeath === undefined ? currentYear : oldest.yearOfDeath;
+        pYearOfDeath = person.yearOfDeath === undefined ? currentYear : person.yearOfDeath
+        if ((oldYearOfDeath - oldest.yearOfBirth) < (pYearOfDeath - person.yearOfBirth)) {
+            return person;
+        }
+        return oldest;
+    })
 };
 
 // Do not edit below this line
